c38ba4e2d861ee9a3e3af2e105e144e3a3f1af93
Faustino-solis-ruiz <79876062+Faustino-solis-ruiz@users.noreply.github.com>
2021-03-19 14:27:25 -0600

    Add files via upload

---
+++ ex_1_1.py
@@ -0,0 +1,10 @@
+print('pepito')
+print('cumpleaños: 22 de enero')
+edad = 42
+print('tengo', edad, 'años')
+cantante = 'suzanne vega'
+comida = 'rucula'
+ciudad = 'Barcelona'
+print('Me gusta la musica de', cantante)
+print('Me gusta cenar', comida)
+print('vivo en', ciudad)
